3869b2e837e1747421e078a0d2083ccadab0899a
Vladzimir Chvei <chveyvladzimir@gmail.com>
2024-07-14 14:47:31 +0300

    first

---
+++ src/main/java/com/worksheet/WorksheetApplication.java
@@ -0,0 +1,11 @@
+package com.worksheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksheetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WorksheetApplication.class,args);
+    }
+}
